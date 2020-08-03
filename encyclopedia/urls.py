95a7e78198f2f200f8f8317e7dbb29de8db17b35
Vineet Soni <vineetsoni17@gmail.com>
2020-08-03 22:48:29 +0530

    first commit with post method, some problem with aww

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name="wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.title, name="title"),
-    path("?q=<str:searchs>", views.query, name="query")
-]
+    path("wiki/<str:name>", views.title, name="title")
+    ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,30 +4,34 @@ from django.http import HttpResponse
 
 from . import util
 
+class SearchQuery(forms.Form):
+    q=forms.CharField(help_text="Search Encyclopedia")
+
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
-        "title": "All Pages"
+    if request.method== "POST":
+        query=request.POST.get("q")
+        entries=[]
+        aww=util.list_entries
+        for stri in aww:
+            if (stri.lowercase()).find(query.lowercase())!=-1:
+                entries.append(stri)
+        return render(request, "encyclopedia/index.html", {
+            "entries": entries,
+            "title": f"Search Result: {query.capitalize()}",
+            "form": SearchQuery(request.POST)
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "title": "All Pages",
+            "form": SearchQuery()
     })
 
 def title(request, name):
     return render(request, "encyclopedia/title.html", {
         "name": name,
-        "body": util.get_entry(name)
-    })
-
-def query(request):
-    awww=util.list_entries()
-    search=(request.GET).clean_data["searchs"]
-    entries=[]
-    for str in awww:
-        if str.find(search)==-1:
-            break
-        else:
-            entries.append(str)
-    return render(request, "encyclopedia/index.html",{
-        "entries": entries,
-        "title": f"Search Result: {search}"
+        "body": util.get_entry(name),
+        "form": SearchQuery()
     })
 
 
